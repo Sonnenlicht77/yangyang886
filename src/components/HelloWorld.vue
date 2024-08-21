a3143c619d8a90fe4038001dc82da87da83e461c
yangyang993 <sonnenlicht@foxmail.com>
2024-08-21 16:10:19 +0800

    初始化

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+<template>
+    <div class="helloworld">
+        <h2>helloworld</h2>
+    </div>
+</template>
+<style scoped lang="less"></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h2>home page</h2>
+</template>
